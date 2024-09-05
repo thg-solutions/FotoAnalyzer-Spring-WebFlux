b8052f3955cd8ae39f0a49f49f67cb27fb1e96eb
Thomas Grund <thomas.grund@thg-solutions.de>
2024-09-05 11:10:57 +0200

    added missing files

---
+++ src/main/java/de/thg/spring/webflux/fotoanalyzer/model/AlbumParams.java
@@ -0,0 +1,24 @@
+package de.thg.spring.webflux.fotoanalyzer.model;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class AlbumParams {
+
+	private String target;
+
+	private List<String> sources;
+
+	private boolean debug;
+
+	public List<String> getSources() {
+		if (sources == null) {
+			sources = new ArrayList<>();
+		}
+		return sources;
+	}
+
+}
