77ff8d259350d45c77699b53d50c1343f7e68d5f
Thomas Grund <thomas.grund@thg-solutions.de>
2024-09-05 11:19:58 +0200

    added missing files

--- src/test/java/de/thg/webflux/fotoanalyzer/FotoanalyzerWebfluxApplicationTests.java
+++ src/test/java/de/thg/webflux/fotoanalyzer/FotoanalyzerWebfluxApplicationTests.java
@@ -1,10 +1,10 @@
-/*
 package de.thg.webflux.fotoanalyzer;
 
+import de.thg.spring.webflux.fotoanalyzer.FotoanalyzerWebfluxApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes= FotoanalyzerWebfluxApplication.class)
 class FotoanalyzerWebfluxApplicationTests {
 
 	@Test
@@ -12,4 +12,3 @@ class FotoanalyzerWebfluxApplicationTests {
 	}
 
 }
-*/
